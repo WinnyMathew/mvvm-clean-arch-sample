aa4b3400771a6ef51b771162ac3e63e3bab8552c
Winny Mathew <winmathe@WKMZTB72ED53.global.publicisgroupe.net>
2024-03-05 14:26:52 +0530

    Refactored the code

--- presentation/src/main/java/com/meals/presentation/ui/categoryList/CategoryListViewModel.kt
+++ presentation/src/main/java/com/meals/presentation/ui/categoryList/CategoryListViewModel.kt
@@ -10,7 +10,6 @@ import com.meals.presentation.ui.categoryList.CategoryScreenIntent.GetMealCatego
 import com.meals.presentation.ui.mapper.categoryUi.CategoryUiMapper
 import com.meals.presentation.utils.CoroutineContextProvider
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharedFlow
